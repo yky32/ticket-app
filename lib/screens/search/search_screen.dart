b5a3fc8b5c2f55ee4ea6438a163a7e0a2bd6512f
wayne.yu <wayne.yu@altech.hk>
2024-12-24 01:40:08 +0800

    add detail tab

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ticket_app/base/res/styles/app_styles.dart';
+import 'package:ticket_app/screens/search/widgets/app_ticket_tabs.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
@@ -16,8 +17,12 @@ class SearchScreen extends StatelessWidget {
             "What are\nyou looking for?",
             style: AppStyles.headLineStyle1.copyWith(),
           ),
+          const SizedBox(height: 20,),
+          const AppTicketTabs(),
         ],
       ),
     );
   }
 }
+
+// 124
---
+++ lib/screens/search/widgets/app_ticket_tabs.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class AppTicketTabs extends StatelessWidget {
+  const AppTicketTabs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: const Color(0xFFF4F6FD)),
+      child: const Row(
+        children: [
+          AppTabs(
+            tabText: "All Tickets",
+          ),
+          AppTabs(
+            tabText: "Hotels",
+            tabBorder: true,
+            tabColor: true,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class AppTabs extends StatelessWidget {
+  const AppTabs(
+      {super.key,
+      this.tabText = "",
+      this.tabBorder = false,
+      this.tabColor = false});
+
+  final String tabText;
+  final bool tabBorder;
+  final bool tabColor;
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 7),
+      width: size.width * 0.44,
+      decoration: BoxDecoration(
+          color: tabColor == false ? Colors.white : Colors.transparent,
+          borderRadius: tabBorder == false
+              ? const BorderRadius.horizontal(left: Radius.circular(50))
+              : const BorderRadius.horizontal(right: Radius.circular(50))),
+      child: Center(child: Text(tabText)),
+    );
+  }
+}

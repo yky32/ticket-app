4e7d34bfa43496ab8d46aa09d16846ddf45293a3
wayne.yu <wayne.yu@altech.hk>
2024-12-31 02:23:59 +0800

    add barcode and myown ticket

--- lib/screens/ticket/ticket_screen.dart
+++ lib/screens/ticket/ticket_screen.dart
@@ -1,3 +1,4 @@
+import 'package:barcode_widget/barcode_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:ticket_app/base/res/media.dart';
 import 'package:ticket_app/base/res/styles/app_styles.dart';
@@ -32,6 +33,7 @@ class TicketScreen extends StatelessWidget {
           const SizedBox(
             height: 20,
           ),
+          // White and Black Ticket
           Container(
               padding: const EdgeInsets.only(left: 16),
               child: TicketView(
@@ -110,12 +112,23 @@ class TicketScreen extends StatelessWidget {
                       children: [
                         Row(
                           children: [
-                            Image.asset(AppMedia.visa_card, scale: 30,),
-                            Text(" *** 2462", style: AppStyles.headLineStyle3,)
+                            Image.asset(
+                              AppMedia.visa_card,
+                              scale: 30,
+                            ),
+                            Text(
+                              " *** 2462",
+                              style: AppStyles.headLineStyle3,
+                            )
                           ],
                         ),
-                        SizedBox(height: 5,),
-                        Text("Payment Method", style: AppStyles.headLineStyle4,)
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          "Payment Method",
+                          style: AppStyles.headLineStyle4,
+                        )
                       ],
                     ),
                     const AppColumnTextLayout(
@@ -128,7 +141,40 @@ class TicketScreen extends StatelessWidget {
                 ),
               ],
             ),
-          )
+          ),
+          const SizedBox(height: 1,),
+          // Bottom of the ticket detail section
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 15),
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            decoration: BoxDecoration(
+                color: AppStyles.ticketColor,
+                borderRadius: const BorderRadius.only(
+                  bottomRight: Radius.circular(21),
+                  bottomLeft: Radius.circular(21),
+                )),
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: BarcodeWidget(
+                  height: 70,
+                  barcode: Barcode.code128(),
+                  data: 'https://www.google.com',
+                  drawText: false,
+                  style: AppStyles.textStyle,
+                  width: double.infinity,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20,),
+          // Colorful for ticket
+          Container(
+              padding: const EdgeInsets.only(left: 16),
+              child: TicketView(
+                ticket: tickets[0],
+              )),
         ],
       ),
     );
